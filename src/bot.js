a7972c80bb7465356e49989c2d9966e28d61a008
David <deebenchimol@gmail.com>
2018-06-30 19:34:02 +0300

    truffle config and started working on web3

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,11 @@
 const token = process.env.TOKEN
 const Bot = require('node-telegram-bot-api')
+const Web3 = require(‘web3’)
+const abi = (require('../build/contracts/EscrowController.json')).abi
+var web3 = new Web3()
+web3.setProvider(new web3.providers.HttpProvider("http://localhost:8545"))
+
+const EscrowControllerContract = web3.eth.contract(abi, process.env.CONTRACT_ADDRESS)
 
 let bot
 
